721d5826700ec153254ccfcf3fe48f74b5350ad8
Michelle Ho <michelle.y.ho@gmail.com>
2025-09-10 15:18:10 -0700

    update addition test

--- test_addition.sh
+++ test_addition.sh
@@ -5,12 +5,13 @@ set -e
 echo "Hello, World!"
 num1=99
 num2=55
-expected_total=$((num1 + num2))
+computed_total=$((num1 + num2))
+expected_total=154
 
-if [ $expected_total -eq 154 ]; then
+if [ $expected_total -eq $computed_total ]; then
   echo "Addition passed"
   exit 0
 else
-  echo "Addition failed"
+  echo "Addition failed. Expected $expected_total. Got $computed_total"
   exit 1
 fi 
