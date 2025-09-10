3031f085e37cdf3c9333dbf7667316454befdf77
Michelle Ho <michelle.y.ho@gmail.com>
2025-09-10 15:21:32 -0700

    update subtraction test

--- test_subtraction.sh
+++ test_subtraction.sh
@@ -4,9 +4,10 @@ set -e
 
 num1=99
 num2=55
-expected_total=$((num1 - num2))
+computed_total=$((num1 - num2))
+expected_total=44
 
-if [ $expected_total -eq 44 ]; then
+if [ $expected_total -eq $computed_total ]; then
   echo "Subtraction passed"
   exit 0
 else
